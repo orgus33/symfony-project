ca7e5849924b4fdaefe0128b415e82e17333c822
Bastien <bastien.b200405@gmail.com>
2022-04-08 18:10:01 +0200

    Première page

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class TestController
 {
@@ -13,10 +14,14 @@ class TestController
         die();
     }
 
-    public function test(Request $request): Response
-    {
-        $age = $request->query->get('age', 0);
 
+    /**
+     * @Route("/test/{age<\d+>?0}", name="test", methods={"GET", "POST"}, shemes={"http", "https"})
+     * 
+     */
+
+    public function test($age): Response
+    {
         return new Response("Vous avez $age ans !");
     }
 }
