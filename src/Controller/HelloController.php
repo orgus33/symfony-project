1e0f801aa574187e2fdfbe9ef98a8161d410de06
Bastien <bastien.b200405@gmail.com>
2022-04-22 22:52:06 +0200

    add HelloController

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,16 +2,27 @@
 
 namespace App\Controller;
 
+use App\Taxes\Calculator;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController
 {
+    protected $calculator;
+
+
+    public function __construct(Calculator $calculator)
+    {
+        $this->calculator = $calculator;
+    }
+
     /**
      * @Route("/hello/{name?World}", name="hello")
      */
     public function hello($name): Response
     {
-        return new Response("Hello $name !");
+        $tva = $this->calculator->calcul(100);
+        return new Response("Hello $name ! Prix : $tva");
     }
 }
