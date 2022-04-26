3f97bed3e6d5b3e6af0bf8134242463bc9076f9a
Bastien <bastien.b200405@gmail.com>
2022-04-26 23:49:50 +0200

    Add some new features

---
+++ src/Controller/ConferenceController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ConferenceController extends AbstractController
+{
+    #[Route('/', name: 'app_conference')]
+    public function index(): Response
+    {
+        return new Response(
+            <<<EOF
+<html>
+    <body>
+        <img src="images\download.gif" />
+    </body>
+</html>
+EOF
+        );
+    }
+}
